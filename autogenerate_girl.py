2096ffeceaa75f01791b19f01779bcb2eb233a9d
noda-sin <noda.sin@gmail.com>
2023-03-26 20:42:36 +0900

    Randmize lora

--- autogenerate_girl.py
+++ autogenerate_girl.py
@@ -6,6 +6,7 @@ import remote_config
 import sys
 import argparse
 import copy
+import random
 
 def is_launched():
     try:
@@ -28,12 +29,22 @@ def generate_girl_txt(gpt_token):
     print(f"generated txt by chatGPT: {txt}")
     return txt
 
+def lora():
+    jp = random.random()
+    kr = random.random()
+    tj = random.random()
+    sum = jp + kr + tj
+    jp = jp / sum * 0.3
+    kr = kr / sum * 0.3
+    tj = tj / sum * 0.3
+    return f"<lora:japaneseDollLikeness_v10:{jp}>,<lora:koreanDollLikeness_v15:{kr}>,<lora:taiwanDollLikeness_v10:{tj}>,"
+
 def generate_girl(gpt_token):
     try:
         txt = generate_girl_txt(gpt_token)
         quote = chat_gpt.generate_quote(gpt_token, txt)
         params = remote_config.config().copy()
-        params["prompt"] = params["prompt"] + f"(({txt}))"
+        params["prompt"] = params["prompt"] + f"{lora()}(({txt}))"
         print("Start to generate girl", params)
         resp = requests.post(url="http://127.0.0.1:7860/sdapi/v1/txt2img", json=params)
         if resp.status_code == 200:
