d44016c7aed96bb3a41d2528f3c21cdd35134cf5
noda-sin <noda.sin@gmail.com>
2023-03-26 11:01:14 +0900

    Add shot parameters

--- chat_gpt.py
+++ chat_gpt.py
@@ -5,18 +5,20 @@ import argparse
 queue = []
 
 prompt = """
-I will write a beautiful girl in AI and post it on Twitter every day. Each time we will change different background, clothes, expression, hairstyle, hair color, camera angle and pose. Can you think of a subject and suggest one?
+I will write a beautiful girl in AI and post it on Twitter every day. Each time we will change different background, clothes, expression, hairstyle, hair color, camera angle, shot and pose.
+Shot is one of them (a close-up of face shot, an upper shot, a full body shot, shot from above, shot from below).
+Can you think of a subject and suggest one?
 Suggestions should be comma-separated and each item should be in English (word for word expressions, if at all possible).
 You may use fictional backgrounds and clothing from games, anime, etc.
 
-例1: Magical floating island background, fantasy RPG mage robe, focused spellcasting, twin-tail hair, purple and blue gradient hair, top-down angle, wielding magic wand
-例2: Underwater city background, futuristic underwater suit, curious smile, see-through bangs, translucent blue hair, diagonal front angle, swimming underwater
-例3: Nighttime food stall background, casual summer outfit, appetizing expression, side braid hair, honey blonde hair, profile angle, holding takoyaki
-例4: Cafe terrace background, casual spring outfit, cheerful smile, bob cut hair, caramel brown hair, diagonal front angle, holding a coffee cup
-例5: Snowy forest background, warm winter coat, wide-eyed surprise, braided hair, ice blue hair, diagonal rear angle, holding a snowflake
-例6: Treetop village background, elf warrior armor, brave stance, braided long hair, forest green hair, front angle, holding a bow
-例7: Desert oasis background, ethnic dress, dreamy expression, long wavy hair, sandy gold hair, diagonal front angle, shading forehead with hand
-例8: Space station background, astronaut suit, serious gaze, short cut hair, silver hair, profile angle, operating spacecraft control panel
+例1: Magical floating island background, fantasy RPG mage robe, focused spellcasting, twin-tail hair, purple and blue gradient hair, top-down angle, a close-up of face shot, wielding magic wand
+例2: Underwater city background, futuristic underwater suit, curious smile, see-through bangs, translucent blue hair, diagonal front angle, a close-up of eyes shot, swimming underwater
+例3: Nighttime food stall background, casual summer outfit, appetizing expression, side braid hair, honey blonde hair, profile angle, a head shot, holding takoyaki
+例4: Cafe terrace background, casual spring outfit, cheerful smile, bob cut hair, caramel brown hair, diagonal front angle, an upper shot, holding a coffee cup
+例5: Snowy forest background, warm winter coat, wide-eyed surprise, braided hair, ice blue hair, diagonal rear angle, a full body shot, holding a snowflake
+例6: Treetop village background, elf warrior armor, brave stance, braided long hair, forest green hair, front angle, shot from above, holding a bow
+例7: Desert oasis background, ethnic dress, dreamy expression, long wavy hair, sandy gold hair, diagonal front angle, shot from below, a full body shot, shading forehead with hand
+例8: Space station background, astronaut suit, serious gaze, short cut hair, silver hair, profile angle, an upper shot, operating spacecraft control panel
 
 Suggestions should be written after 'A:'.
 """
